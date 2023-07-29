1d0f7706236ecfadc1440d798ca575c66f1b534e
baqaB714 <baqa4731@gmail.com>
2023-07-29 18:56:36 +0330

    first commit 14020507

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Content from './Content';
-import { Mcontext } from './contexts/Mcontext';
-import Sidebar from './Sidebar';
 
-const App = ()=>{
+const App = () => {
 
-    const [showMenu , setShowMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
 
     return (
-        <div>
-            <BrowserRouter>
-                <Mcontext.Provider value={{showMenu, setShowMenu}}>
-                    <Sidebar/>
-                    <Content/>            
-                </Mcontext.Provider>
-            </BrowserRouter>
-                
+        <div className='body'>
+
+            <div className='top_menu bg-light w-100'>
+                <div className='menu_icon container row p-3'>
+                    <a className='pointer col-md-2 col-4 d-flex flex-column align-items-center pt-3 text-decoration-none'>
+                        <i className='fas fa-user-plus fs-4'></i>
+                        <span className=''>افزودن شاگرد</span>
+                    </a>
+                    <a className='pointer col-md-2 col-4 d-flex flex-column align-items-center pt-3 text-decoration-none'>
+                        <i className='fas fa-door-open fs-4'></i>
+                        <span >ثبت ورود و خروج</span>
+                    </a>
+                    <a className='pointer col-md-2 col-4 d-flex flex-column align-items-center pt-3 text-decoration-none'>
+                        <i className='fas fa-money-check-alt fs-4'></i>
+                        <span >ثبت شهریه</span>
+                    </a>
+                </div>
+            </div>
+
+            <div className='main bg-dark'>
+            </div>
+
+
         </div>
-    ) 
+    )
 }
 
 
